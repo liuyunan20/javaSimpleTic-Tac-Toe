7545cc70275d190c403f49b799277423ff9b9f69
liuyunan <liuyunan.20@gmail.com>
2022-08-04 11:19:35 -0700

    In this stage, you will write a program that:
    Reads a string of 9 symbols from the input and displays them to the user in a 3x3 grid. The grid can contain only X, O and _ symbols.
    Outputs a line of dashes --------- above and below the grid, adds a pipe | symbol to the beginning and end of each line of the grid, and adds a space between all characters in the grid.
    Examples
    The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
    
    Example 1:
    
    > O_OXXO_XX
    ---------
    | O _ O |
    | X X O |
    | _ X X |
    ---------
    Example 2:
    
    > OXO__X_OX
    ---------
    | O X O |
    | _ _ X |
    | _ O X |
    ---------
    Example 3:
    
    > _XO__X___
    ---------
    | _ X O |
    | _ _ X |
    | _ _ _ |
    ---------

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,21 @@
 package tictactoe;
+import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("X O X");
-        System.out.println("X O X");
+        Scanner scanner = new Scanner(System.in);
+        String symbols = scanner.next();
+        String dashes = "---------";
+        System.out.println(dashes);
+        for (int i = 0; i < 3; i++) {
+            String line = "| ";
+            for (int j = 0; j < 3; j++) {
+                line += symbols.charAt(j + 3 * i) + " ";
+            }
+            line += "|";
+            System.out.println(line);
+        }
+        System.out.println(dashes);
     }
 }
