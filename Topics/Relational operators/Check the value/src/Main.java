7321ce1b95ec426952c81577c6204f2b8dae1364
liuyunan <liuyunan.20@gmail.com>
2022-08-05 19:57:01 -0700

    Check the value Write a program that reads an integer value and checks if it is less than 10 and greater than 0.
    Your program should print a boolean value (true or false).
    
    Sample Input:
    0
    
    Sample Output:
    false

---
+++ Topics/Relational operators/Check the value/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n > 0 && n < 10) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
