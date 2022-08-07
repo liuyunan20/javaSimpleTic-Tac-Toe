80feda9ea51597bdf3ab3881a04cfd2476377913
liuyunan <liuyunan.20@gmail.com>
2022-08-06 23:26:05 -0700

    Write a program that inputs a sequence of integer numbers in a loop and calculates their sum under the following conditions:
    1) if a new number is 0, the program must stop the loop and output the accumulated sum;
    
    2) if the sum is equal to or exceeds the limit of 1000, the program must also stop reading the numbers and output the value equal to the sum minus 1000.
    
    Note: the input can contain extra numbers. Just ignore them.
    
     Report a typo
    Sample Input 1:
    
    800
    101
    102
    300
    0
    Sample Output 1:
    
    3
    Sample Input 2:
    
    103
    105
    109
    0
    1000
    Sample Output 2:
    
    317

---
+++ Topics/Branching statements/The integer barrier/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        while (n != 0) {
+            sum += n;
+            if (sum >= 1000) {
+                sum -= 1000;
+                break;
+            }
+            n = scanner.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
