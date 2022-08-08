81e8ebe0adcab9679c8cf7467f0284e5e08c3438
liuyunan <liuyunan.20@gmail.com>
2022-08-07 21:23:11 -0700

    Write a program that reads an array of ints named a and cyclically shift the elements of the array to the right: a[0] goes to the place of a[1], a[1] goes to the place of a[2], ..., and the last element goes to the place of a[0]).
    Input data format
    
    The first line of the input contains the number of elements in the array. The second line contains the elements of the array.
    
    Output data format
    
    The single line should contain all shifted elements of the array. Elements must be separated by the space character.
    
    Note that If your solution gets the code quality warning "System.arraycopy is more efficient", please simply ignore it in respect of this code challenge.

---
+++ Topics/Iterating over arrays/Cyclically shifting elements/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int num = a[n - 1];
+        for (int i = n - 1; i > 0; i--) {
+            a[i] = a[i - 1];
+        }
+        a[0] = num;
+        for (int i = 0; i < n - 1; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.print(a[n - 1]);
+    }
+}
